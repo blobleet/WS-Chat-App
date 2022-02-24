5761be4e7653272e2ebc68703a77d378e6a94a2f
Chris <83955174+blobleet@users.noreply.github.com>
2022-02-24 16:09:09 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/wschatapp/MainActivity.kt
+++ app/src/main/java/com/example/wschatapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.wschatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -19,9 +21,13 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var lilBtn: Button
-    private lateinit var lilET: EditText
+    private lateinit var sendBtn: Button
+    private lateinit var connectBtn: Button
+    private lateinit var etMessage: EditText
+    private lateinit var etName: EditText
+    private lateinit var messagesLayout: LinearLayout
     var message = ""
+    private lateinit var myLayout: View
 
     private val client = HttpClient(CIO) {
         install(WebSockets)
@@ -30,20 +36,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lilBtn = findViewById(R.id.button)
-        lilET = findViewById(R.id.editText)
+        sendBtn = findViewById(R.id.btn_send)
+        connectBtn = findViewById(R.id.btn_connect)
+        etMessage = findViewById(R.id.et_message)
+        etName = findViewById(R.id.et_name)
+        messagesLayout = findViewById(R.id.messages_layout)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            runClient()
+        connectBtn.setOnClickListener {
+            if(etName.text.isBlank()) {
+                addText("Enter your name before connecting")
+            } else {
+                CoroutineScope(Dispatchers.IO).launch {
+                    runClient(etName.text.toString())
+                }
+            }
         }
 
-        lilBtn.setOnClickListener {
-            message = lilET.text.toString()
-            lilET.text.clear()
+        sendBtn.setOnClickListener {
+            message = etMessage.text.toString()
+            etMessage.text.clear()
         }
     }
 
-    private suspend fun runClient() {
+    private suspend fun runClient(name: String) {
         client.webSocket(HttpMethod.Get, "192.168.182.174", 8080, "/wschat") {
             val messageOutputRoutine = launch { outputMessages() }  // Launch output coroutine
             val userInputRoutine = launch { inputMessages() }  // Launch input coroutine
@@ -60,7 +75,11 @@ class MainActivity : AppCompatActivity() {
         try {
             for(message in incoming) {
                 message as? Frame.Text ?: continue
-                Log.d(TAG, message.readText())
+                val messageText = message.readText()
+                Log.d(TAG, "Received message: $messageText")
+                runOnUiThread{
+                    addText(messageText)
+                }
             }
         } catch (e: Exception) {
             Log.e(TAG, "outputMessages: Error receiving message\n${e.message}")
@@ -70,8 +89,8 @@ class MainActivity : AppCompatActivity() {
     private suspend fun DefaultClientWebSocketSession.inputMessages() {
         while(true){
             if (message == "") continue
-            if (message.equals("exit", true)) return
             try {
+                Log.d(TAG, "Sending message: $message")
                 send(message)
                 message = ""
             } catch (e: Exception){
@@ -79,5 +98,14 @@ class MainActivity : AppCompatActivity() {
                 return
             }
         }
+    }
+
+    private fun addText(text: String){
+        myLayout = layoutInflater.inflate(
+            R.layout.received_message,
+            messagesLayout,
+            false)
+        myLayout.findViewById<TextView>(R.id.message).text = text
+        messagesLayout.addView(myLayout)
     }
 }
